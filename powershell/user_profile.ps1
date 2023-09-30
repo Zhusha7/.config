ace9983e7a78c956a83ca2349ac8188b53812536
Zhusha7 <andzej200@gmail.com>
2023-10-01 02:27:41 +0300

    Changed to UEW theme

--- powershell/user_profile.ps1
+++ powershell/user_profile.ps1
@@ -2,8 +2,12 @@
 function ll_alias {lsd.exe -la}
 Set-Alias -Name ls -Value lsd -Option AllScope
 Set-Alias -Name ll -Value ll_alias -Option AllScope
+Set-Alias -Name la -Value ll_alias -Option AllScope
 Set-Alias vim nvim
 Set-Alias g git
 Set-Alias grep findstr
 Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
 Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+
+#INITS
+oh-my-posh init pwsh --config 'C:\Users\Zhusha\scoop\apps\oh-my-posh\current\themes\uew.omp.json' | Invoke-Expression
